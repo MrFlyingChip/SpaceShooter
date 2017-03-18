4e45c1c08dbb3471e23b4047880d0a38b2524802
MrFlyingChip <kv_l@ukr.net>
2017-03-18 16:46:45 +0200

    Done project

--- Space Shooter/Assets/Scripts/EvasiveManeuver.cs
+++ Space Shooter/Assets/Scripts/EvasiveManeuver.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class EvasiveManeuver : MonoBehaviour {
 
+    public float dodge;
+    public float smoothing;
+    public float tilt;
+    public Vector2 startWait;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    public Boundary boundary;
+    private float currentSpeed;
+    private float targerManeuver;
+    private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine(Evade());
 	}
 	
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+
+        while (true)
+        {
+            targerManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targerManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+    {
+        float newManeuver = Mathf.MoveTowards(rb.velocity.x, targerManeuver, Time.deltaTime * smoothing);
+        rb.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
 	}
 }
